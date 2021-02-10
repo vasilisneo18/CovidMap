e3ff020bf28dd0cd6094545c486d2fd953286973
vasilisneophytou <vasilisneo18@HOTMAIL.COM>
2021-02-10 13:43:16 +0200

    Initial Commit

---
+++ CovidMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidMap
+//
+//  Created by Vasilis Neophytou on 10/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
